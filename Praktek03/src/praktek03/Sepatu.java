f534594336915b086c04f140371bca81837f422e
BahranIlmi <bahranilmi13@gmail.com>
2018-04-07 15:18:50 +0800

    Buat Getter and Setter

--- Praktek03/src/praktek03/Sepatu.java
+++ Praktek03/src/praktek03/Sepatu.java
@@ -10,4 +10,30 @@ public class Sepatu {
                 "Warna \t: "+warna+"\n"+
                 "Harga \t: "+harga+"\n");
     }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
 }
