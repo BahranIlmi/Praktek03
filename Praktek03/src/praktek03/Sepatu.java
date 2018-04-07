9f0cfcf57a57400e33af7f4d705c893317cd1937
BahranIlmi <bahranilmi13@gmail.com>
2018-04-07 15:13:18 +0800

    Buat Method cetakInfo()

--- Praktek03/src/praktek03/Sepatu.java
+++ Praktek03/src/praktek03/Sepatu.java
@@ -3,4 +3,11 @@ public class Sepatu {
     String jenis;
     String warna;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("====================");
+        System.out.println("Jenis \t: "+jenis+"\n"+
+                "Warna \t: "+warna+"\n"+
+                "Harga \t: "+harga+"\n");
+    }
 }
