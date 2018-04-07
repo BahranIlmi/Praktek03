88675d361af100afdd50426a6f26321620390d4d
BahranIlmi <bahranilmi13@gmail.com>
2018-04-07 15:16:47 +0800

    Instance, Buat Atribut dan Panggil Method

--- Praktek03/src/praktek03/SepatuAksi.java
+++ Praktek03/src/praktek03/SepatuAksi.java
@@ -1,6 +1,24 @@
 package praktek03;
 public class SepatuAksi {
     public static void main(String[] args){
+        Sepatu Vans = new Sepatu();
+        Sepatu Nike = new Sepatu();
+        Sepatu Adidas = new Sepatu();
         
+        Vans.jenis="Sneakers";
+        Vans.warna="Hitam";
+        Vans.harga=300000;
+        
+        Nike.jenis="Running";
+        Nike.warna="Navy";
+        Nike.harga=500000;
+        
+        Adidas.jenis="Boots";
+        Adidas.warna="Hitam";
+        Adidas.harga=800000;
+        
+        Vans.cetakInfo();
+        Nike.cetakInfo();
+        Adidas.cetakInfo();
     }
 }
